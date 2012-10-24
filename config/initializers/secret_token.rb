1bbade3636b3d6ffef9b9abd774a7409d004e002
wong lee <write.wonglee@gmail.com>
2012-10-24 15:39:34 -0400

    starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movementplayer::Application.config.secret_token = '75825ad0ccafd5745e881aec8572a236e2092fda800e00acfe1dd3afd3955e146ed7622bf4c380b9d2c00724d3d934ae95f2933aac3269057e04a89c483202d0'
