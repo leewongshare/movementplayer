2c652ad3016e979122989dfdd48766530093741c
wong lee <write.wonglee@gmail.com>
2012-10-24 17:51:52 -0400

    finished functionality for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u1 = User.create(:name => "wong lee1", :email => "lee1@lee.com",:password => "aaa")
+u2 = User.create(:name => "wong lee2", :email => "lee2@lee.com",:password => "aaa")
+u3 = User.create(:name => "wong lee3", :email => "lee3@lee.com",:password => "aaa")
+
+u1.save
+u2.save
+u3.save
+
