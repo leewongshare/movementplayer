166b4299609779c5345325efef0f2f769ea50026
wong lee <write.wonglee@gmail.com>
2012-10-25 00:56:16 -0400

    added Songs functionality; Amazon S3 working for all controller actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,75 @@
 class SongsController < ApplicationController
 
-  def index
+    def index
+    @songs = Song.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @songs }
+    end
+  end
+
+  def new
+    @song = Song.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @song }
+    end
+  end
+
+  def edit
+
+      @song = Song.find(params[:id])
+
+  end
+
+  def update
+      @song = Song.find(params[:id])
+      @song.assign_attributes(params[:song])
+
+
+      if @song.save
+        redirect_to songs_path
+      else
+        render :edit
+      end
+  end
+
+  def show
+
+    @song = Song.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @song }
+    end
+
+  end
+
+  def create
+    @song = Song.new(params[:song])
+
+    respond_to do |format|
+      if @song.save
+        format.html { redirect_to @song, notice: 'Song was successfully created.' }
+        format.json { render json: @song, status: :created, location: @song }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    respond_to do |format|
+      format.html { redirect_to songs_url }
+      format.json { head :no_content }
+    end
 
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,18 @@ class UsersController < ApplicationController
 
   end
 
+  def update
+      @user = User.find(params[:id])
+      @user.assign_attributes(params[:user])
+
+
+      if @user.save
+        redirect_to users_path
+      else
+        render :edit
+      end
+  end
+
   def show
 
     @user = User.find(params[:id])
